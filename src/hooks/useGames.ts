b827da9652c602d4297b79bbb3acfb44519dbbba
Tudor Dragomir <ttdragomir@gmail.com>
2023-12-18 16:47:37 +0200

    Refactor entities

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,7 +2,7 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 import ms from "ms";
 import useGameQueryStore from "../gameQueryStore";
 import APIClient, { FetchResponse } from "../services/api-client";
-import { Game } from "../entities/Game";
+import Game from "../entities/Game";
 
 const apiClient = new APIClient<Game>("/games");
 
